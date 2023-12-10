fe10bfcccdfa5cde2caeb1d4a648615339bdddcd
Quinten Preiß <preissquinten@gmail.com>
2023-12-10 15:33:39 +0100

    cover

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+export default function Hero() {
+  return (
+    <div className="w-full h-screen relative overflow-hidden ">
+      <Image
+        className="object-cover object-left-top  "
+        src="/hero.jpg"
+        fill={true}
+        alt="hero"
+      ></Image>
+      <div className="absolute top-[50vh] left-0 w-full h-[50vh] bg-gradient-to-b from-transparent to-black">
+        <div className="w-full h-full flex flex-col justify-start items-end px-40">
+          <h1 className="text-white text-6xl font-bold">Jonathan Henrich</h1>
+          <p className="text-white text-2xl font-bold">
+            musician • songwriter • performer • producer
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function Navbar() {
+  return (
+    <div className="w-full h-16 flex flex-row gap-10 justify-end px-10 fixed top-0 z-20">
+      <Link
+        href="https://www.instagram.com/jonathan.henrich/"
+        className="text-white my-auto z-20"
+      >
+        <Image
+          className="filter-invert"
+          src="/instagram.png"
+          width="16"
+          height="16"
+          alt="instagram"
+        ></Image>
+      </Link>
+      <Link
+        href="https://open.spotify.com/intl-de/artist/139HzhvBYPiCmliF0jkjEl?si=6MP4SqF6RVyyQqEpE7o9rw"
+        className="text-black my-auto"
+      >
+        <Image src="/spotify.png" width="16" height="16" alt="spotify"></Image>
+      </Link>
+      <Link href="./" className="text-white my-auto">
+        <span>HOME</span>
+      </Link>
+      <Link href="./" className="text-white my-auto">
+        <span>MUSIC</span>
+      </Link>
+      <Link href="./" className="text-white  my-auto">
+        <span>EPK</span>
+      </Link>
+    </div>
+  );
+}
