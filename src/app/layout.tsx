1c0f66be51f4d97a2bcc0c801170663a551560b1
Quinten Preiß <preissquinten@gmail.com>
2024-01-03 00:41:53 +0100

    speed insights2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <SpeedInsights />
       <body className={inter.className}>
         {children}
         <SpeedInsights />
